8269ef4bd7eb443c42f1bb2d1d4196f5461ab533
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-08 15:30:55 +0800

    use chatkit instead

---
+++ script.js
@@ -0,0 +1,179 @@
+// ==UserScript==
+// @name         chatgpt twice
+// @namespace    https://github.com/mefengl
+// @version      0.0.5
+// @description  ask question twice!
+// @author       mefengl
+// @match        https://chat.openai.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @require      https://cdn.staticfile.org/jquery/3.6.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+(() => {
+    var __async = (__this, __arguments, generator) => {
+        return new Promise((resolve, reject) => {
+            var fulfilled = (value) => {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            };
+            var rejected = (value) => {
+                try {
+                    step(generator.throw(value));
+                } catch (e) {
+                    reject(e);
+                }
+            };
+            var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+            step((generator = generator.apply(__this, __arguments)).next());
+        });
+    };
+
+    // ../../packages/chatkit/dist/index.mjs
+    function getTextarea() {
+        const form = document.querySelector("form");
+        if (!form)
+            return;
+        const textareas = form.querySelectorAll("textarea");
+        const result = textareas[0];
+        return result;
+    }
+    function getSubmitButton() {
+        const form = document.querySelector("form");
+        if (!form)
+            return;
+        const buttons = form.querySelectorAll("button");
+        const result = buttons[buttons.length - 1];
+        return result;
+    }
+    function getRegenerateButton() {
+        const form = document.querySelector("form");
+        if (!form)
+            return;
+        const buttons = form.querySelectorAll("button");
+        const result = Array.from(buttons).find((button) => {
+            var _a;
+            return (_a = button.textContent) == null ? void 0 : _a.trim().toLowerCase().includes("regenerate");
+        });
+        return result;
+    }
+    function getStopGeneratingButton() {
+        const form = document.querySelector("form");
+        if (!form)
+            return;
+        const buttons = form.querySelectorAll("button");
+        const result = Array.from(buttons).find((button) => {
+            var _a;
+            return (_a = button.textContent) == null ? void 0 : _a.trim().toLowerCase().includes("stop generating");
+        });
+        return result;
+    }
+    function getLastResponseElement() {
+        const responseElements = document.querySelectorAll(".group.w-full");
+        return responseElements[responseElements.length - 1];
+    }
+    function getLastResponse() {
+        const lastResponseElement = getLastResponseElement();
+        if (!lastResponseElement)
+            return;
+        const lastResponse = lastResponseElement.textContent;
+        return lastResponse;
+    }
+    function getTextareaValue() {
+        var _a;
+        return ((_a = getTextarea()) == null ? void 0 : _a.value) || "";
+    }
+    function setTextarea(message) {
+        const textarea = getTextarea();
+        if (!textarea)
+            return;
+        textarea.value = message;
+        textarea.dispatchEvent(new Event("input"));
+    }
+    function send(message) {
+        setTextarea(message);
+        const textarea = getTextarea();
+        if (!textarea)
+            return;
+        textarea.dispatchEvent(new KeyboardEvent("keydown", { key: "Enter", bubbles: true }));
+    }
+    function regenerate() {
+        const regenerateButton = getRegenerateButton();
+        if (!regenerateButton)
+            return;
+        regenerateButton.click();
+    }
+    function onSend(callback) {
+        const textarea = getTextarea();
+        if (!textarea)
+            return;
+        textarea.addEventListener("keydown", function (event) {
+            if (event.key === "Enter" && !event.shiftKey) {
+                callback();
+            }
+        });
+        const sendButton = getSubmitButton();
+        if (!sendButton)
+            return;
+        sendButton.addEventListener("mousedown", callback);
+    }
+    function waitForIdle() {
+        return new Promise((resolve) => {
+            const interval = setInterval(() => {
+                if (!getStopGeneratingButton()) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 1e3);
+        });
+    }
+    var chatgpt = {
+        getTextarea,
+        getSubmitButton,
+        getRegenerateButton,
+        getStopGeneratingButton,
+        getLastResponseElement,
+        getLastResponse,
+        getTextareaValue,
+        setTextarea,
+        send,
+        regenerate,
+        onSend,
+        waitForIdle
+    };
+    var chatgpt_default = chatgpt;
+
+    // src/index.ts
+    function initialize() {
+        return __async(this, null, function* () {
+            yield new Promise((resolve) => window.addEventListener("load", resolve));
+            yield new Promise((resolve) => setTimeout(resolve, 1e3));
+        });
+    }
+    function main() {
+        return __async(this, null, function* () {
+            yield initialize();
+            while (true) {
+                yield new Promise((resolve) => setTimeout(resolve, 1e3));
+                const lastResponseElement = chatgpt_default.getLastResponseElement();
+                if (!lastResponseElement) {
+                    continue;
+                }
+                if (lastResponseElement.childElementCount === 1) {
+                    yield new Promise((resolve) => setTimeout(resolve, 1e3));
+                    yield chatgpt_default.waitForIdle();
+                    const duplicatedChild = lastResponseElement.children[0].cloneNode(true);
+                    lastResponseElement.appendChild(duplicatedChild);
+                    chatgpt_default.regenerate();
+                }
+            }
+        });
+    }
+    (function () {
+        return __async(this, null, function* () {
+            yield main();
+        });
+    })();
+})();
